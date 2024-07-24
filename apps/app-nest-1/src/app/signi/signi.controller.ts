69cf202038ca17818e35aa708e8d6098eecae9ff
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-24 22:31:29 +0200

    feat: implement Signi experiments

--- apps/app-nest-1/src/app/signi/signi.controller.ts
+++ apps/app-nest-1/src/app/signi/signi.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
 import { SigniService } from './signi.service';
 
 @Controller('signi')
 export class SigniController {
   constructor(private readonly signiService: SigniService) {}
 
-  @Get()
-  getData() {
-    return this.signiService.getData();
+  @Post('contract')
+  async createContract() {
+    return this.signiService.createContract();
+  }
+
+  @Post('contract/callback')
+  @HttpCode(200)
+  callback(
+    @Query() query: { state: string; templateId: string; sessionId: string },
+    @Body() body: any,
+  ) {
+    console.log(
+      'SigniController.callback query',
+      JSON.stringify(query, null, 2),
+    );
+    console.log('SigniController.callback body', JSON.stringify(body, null, 2));
   }
 }
--- apps/app-nest-1/src/app/signi/signi.service.ts
+++ apps/app-nest-1/src/app/signi/signi.service.ts
@@ -2,7 +2,101 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SigniService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  async createContract() {
+    const webhookUrl = process.env['SIGNI_WEBHOOK_URL']!;
+    const body = {
+      locale: 'sk',
+      people: [
+        {
+          is_proposer: true,
+          email: process.env['SIGNI_PROPOSER_EMAIL'],
+          contract_role: 'notice',
+        },
+        {
+          is_proposer: false,
+          email: process.env['SIGNI_SIGNER_EMAIL'],
+          contract_role: 'sign',
+          phone: process.env['SIGNI_SIGNER_PHONE_NUMBER'],
+          first_name: 'mock-first-name',
+          last_name: 'mock-last-name',
+          person_type: 'citizen',
+        },
+      ],
+      template: {
+        id: process.env['SIGNI_TEMPLATE_ID'],
+        parameters: [
+          {
+            id: 'id',
+            value: 'mock-id',
+          },
+          {
+            id: 'name',
+            value: 'mock-name',
+          },
+          {
+            id: 'address',
+            value: 'mock-address',
+          },
+          {
+            id: 'birthDate',
+            value: 'mock-birthdate',
+          },
+          {
+            id: 'email',
+            value: 'mock-email',
+          },
+          {
+            id: 'phoneNumber',
+            value: process.env['SIGNI_SIGNER_PHONE_NUMBER'],
+          },
+          {
+            id: 'price',
+            value: 'mock-price',
+          },
+          {
+            id: 'programName',
+            value: 'mock-program-name',
+          },
+          {
+            id: 'variableSymbol',
+            value: 'mock-variable-symbol',
+          },
+        ],
+      },
+      webhooks: [
+        {
+          state: 'completed',
+          url:
+            webhookUrl +
+            `/api/signi/contract/callback?state=completed&someQueryParam=someValue`,
+        },
+        {
+          state: 'rejected',
+          url:
+            webhookUrl +
+            `/api/signi/contract/callback?state=rejected&someQueryParam=someValue`,
+        },
+        {
+          state: 'expired',
+          url:
+            webhookUrl +
+            `/api/signi/contract/callback?state=expired&someQueryParam=someValue`,
+        },
+      ],
+    };
+    console.log(JSON.stringify(body, null, 2));
+
+    const response = await fetch('https://api.signi.com/api/v1/contract/', {
+      method: 'POST',
+      headers: {
+        'x-api-key': process.env['SIGNI_API_KEY']!,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const result = await response.json();
+    console.log(JSON.stringify(result, null, 2));
   }
 }
