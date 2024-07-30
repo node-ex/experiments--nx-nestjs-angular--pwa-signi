b67e56b813126d81389524a42802cc030544dcf9
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-30 11:07:53 +0200

    feat: setup serving FE from BE

--- apps/app-nest-1/src/app/app.module.ts
+++ apps/app-nest-1/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SigniModule } from './signi/signi.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import path from 'path';
+import type { Response } from 'express';
 
 @Module({
   imports: [
@@ -14,6 +17,23 @@ import { ConfigModule } from '@nestjs/config';
       // cache: true,
     }),
     SigniModule,
+    // https://docs.nestjs.com/recipes/serve-static
+    // https://github.com/nestjs/serve-static
+    // https://github.com/nestjs/serve-static/blob/master/lib/interfaces/serve-static-options.interface.ts
+    ServeStaticModule.forRoot({
+      rootPath: path.join(
+        process.cwd(),
+        'dist',
+        'apps',
+        'app-angular-1',
+        'browser',
+      ),
+      serveStaticOptions: {
+        setHeaders: (res: Response, url) => {
+          res.set({ 'X-Custom-Header': 'some-value' });
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
