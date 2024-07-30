0b0b09491936fd5dd5bce7faaf93213662a668e8
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-30 11:17:08 +0200

    chore: make app-angular-1 a PWA using @angular/pwa:ng-add generator

--- apps/app-angular-1/src/app/app.config.ts
+++ apps/app-angular-1/src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideHttpClient(),
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
