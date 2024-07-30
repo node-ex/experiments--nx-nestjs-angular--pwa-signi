966778cbdc908e0ab58b4dfacf5c7ab69202a954
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-30 11:19:02 +0200

    chore: remove service worker

--- apps/app-angular-1/src/app/app.config.ts
+++ apps/app-angular-1/src/app/app.config.ts
@@ -1,16 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+    provideHttpClient(),
   ],
 };
